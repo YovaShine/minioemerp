aa831bec04dc955233097f12af929deab6bc8ff9
Jing <shxjing@cn.ibm.com>
2018-04-22 01:46:51 +0800

    first check in with template of shopping site

---
+++ pages/component/index.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    imgUrls: [
+      '/images/b1.jpg',
+      '/images/b2.jpg',
+      '/images/b3.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 3000,
+    duration: 800,
+  }
+})
